0758134475e8f2819f1cb666424693514e57ef8a
Your Name <loposflorin@gmail.com>
2019-06-22 15:27:40 +0300

    first commit

---
+++ src/components/AddButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+        <md-button class="md-primary">Add new task...</md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddButton',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .md-primary {
+    border: 1px solid blue;
+  }
+</style>
