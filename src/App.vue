13f8b61703dca6c41ce0cdd6a8f27d225f0dfbe4
Your Name <loposflorin@gmail.com>
2019-06-29 18:57:00 +0300

    add: on click card creation

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <template>
   <div id="app">
-      <md-button class="md-primary">Add new task...</md-button>
+      <md-button @click="addCard" class="md-primary">Add new task...</md-button>
+      <ul>
+        <li v-for="(card, index) in cards" v-bind:key="cards.id"><card /></li>
+      </ul>
   </div>
 </template>
 
 <script>
+import Card from './components/Card.vue'
 
 export default {
   name: 'app',
   components: {
+    Card
+  },
+  data: function () {
+    return {
+      cards: [
+
+      ],
+      nextCardId: 1
+    }
+  },
+  methods: {
+    addCard: function () {
+      console.log('test')
+      this.cards.push(this.nextCardId++)
+    }
   }
 };
 </script>
 
 <style>
+  body {
+    height: 100%
+  }
+
   #app {
     background: #eea;
   }
+
+  ul {
+    list-style-type: none;
+  }
+  
 </style>
