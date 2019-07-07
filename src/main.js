ffea82181447783bd16aef0b33b31961302c6b66
Your Name <loposflorin@gmail.com>
2019-07-07 15:05:31 +0300

    add: vue-router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
+import VueRouter from 'vue-router';
 import Vuex from 'vuex';
 import App from './App.vue';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 
 Vue.use(VueMaterial);
+Vue.use(VueRouter);
 Vue.use(Vuex);
 
 Vue.config.productionTip = false;
